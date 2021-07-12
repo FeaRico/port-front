03623d66590f521ea05b81a545a05d55965796b4
Mahach Abdulazizov <abdulazizov2001@mail.ru>
2021-07-12 03:17:35 +0300

    Developed logics detail for ships and docks

--- src/app/docks/dock-detail/dock-detail.component.ts
+++ src/app/docks/dock-detail/dock-detail.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+
+import {Dock} from "../dock";
+import {DocksService} from "../docks.service";
 
 @Component({
   selector: 'app-dock-detail',
   templateUrl: './dock-detail.component.html',
-  styleUrls: ['./dock-detail.component.css']
+  styleUrls: ['./dock-detail.component.css'],
+  providers: [DocksService]
 })
 export class DockDetailComponent implements OnInit {
+  dock: Dock | undefined;
 
-  constructor() { }
+  constructor(
+    private dockService: DocksService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getDock();
+  }
+
+  public getDock(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.dockService.getDock(id)
+      .subscribe(result => {
+        this.dock = result;
+        console.log('Dock:');
+        console.log(this.dock);
+      })
   }
 
 }

854889f011e50e252544766e662ca411a0904ba6
Mahach Abdulazizov <abdulazizov2001@mail.ru>
2021-07-12 13:34:40 +0300

    Added a form for adding docks

--- src/app/docks/dock-add/dock-add.component.ts
+++ src/app/docks/dock-add/dock-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from "@angular/common";
+
+import {DocksService} from "../docks.service";
+import {Dock} from "../dock";
 
 @Component({
   selector: 'app-dock-add',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dock-add.component.css']
 })
 export class DockAddComponent implements OnInit {
+  docks: Dock[];
 
-  constructor() { }
+  constructor(
+    private docksService: DocksService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public add(size: any){
+    size = size.trim();
+    size = parseInt(size);
+    let lon = 0;
+    let lat = 0;
+    let amountShips = 0;
+    if(!size){
+      return;
+    }
+    this.docksService.addDock({size, lon, lat, amountShips } as Dock)
+      .subscribe(dock => {
+        console.log(dock);
+      });
+    this.backToPage();
+  }
+
+  public backToPage(): void {
+    this.location.back();
   }
 
 }
